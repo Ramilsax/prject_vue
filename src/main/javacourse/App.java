adc674fa1aba62226e281fb2b97b8287f743f612
Ramil Yarullin <ramil-sax@yandex.ru>
2020-06-01 20:11:31 +0500

    здравствуйте

--- src/main/javacourse/App.java
+++ src/main/javacourse/App.java
@@ -2,6 +2,6 @@ package main.javacourse;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Здравствуйте, здравствуйте");
     }
 }
